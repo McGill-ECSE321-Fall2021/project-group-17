6a198a91850298e4ed9adf51b2a86112f34a249d
Tomasz Mroz <tomasz.mroz@mail.mcgill.ca>
2021-10-09 11:25:36 -0400

    Added person class

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/model/Person.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/model/Person.java
@@ -1,4 +1,18 @@
 package ca.mcgill.ecse321.library.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+    private String name;
+
+    @Id
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
