7c77e96e32c2f00791aec7f4d72205fea6155d65
fionaryan1 <70776631+fionaryan1@users.noreply.github.com>
2021-10-15 15:09:47 -0400

    onlineaccount repo1

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/dao/OnlineAccountRepository.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/dao/OnlineAccountRepository.java
@@ -5,5 +5,4 @@ import ca.mcgill.ecse321.library.model.OnlineAccount;
 
 public interface OnlineAccountRepository extends CrudRepository<OnlineAccount, String>{
     OnlineAccount findOnlineAccountByUsername(String username);
-    OnlineAccount findOnlineAccountByPassword(String password);
 }
