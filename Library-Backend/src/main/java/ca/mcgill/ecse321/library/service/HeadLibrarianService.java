1e41ff81c89f3e4e5abe114b682d83c31ca03a65
Aidan Jackson <aidan.jackson@mail.mcgill.ca>
2021-10-28 09:41:59 -0400

    Fixed HeadLibrarian Service

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/HeadLibrarianService.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/HeadLibrarianService.java
@@ -2,7 +2,6 @@ package ca.mcgill.ecse321.library.service;
 
 import ca.mcgill.ecse321.library.dao.*;
 import ca.mcgill.ecse321.library.model.*;
-import jdk.javadoc.internal.doclets.formats.html.markup.Head;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
