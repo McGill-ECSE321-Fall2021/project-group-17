880f7b302c0c08450d6f34233ddf8cfd8499d2b4
Tomasz Mroz <tomasz.mroz@mail.mcgill.ca>
2021-11-08 15:03:12 -0500

    bash integration test driver clears db automatically

--- Library-Backend/src/test/java/ca/mcgill/ecse321/library/integrationTest/integrationTestDriver.sh
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/integrationTest/integrationTestDriver.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+clearDB () {
+  psql postgres://lwhzzfglqzmohj:caf222c6ec61a394ac34989679eb98c5d432a664e68c5cf3c319199318f74954@ec2-44-198-154-255.compute-1.amazonaws.com:5432/ddsk1hvat3st1k << EOF
+TRUNCATE address,book,customer,head_librarian,item_instance,librarian,library,library_card,library_hour,loan,movie,music,newspaper,online_account,person,reservation,shift
+EOF
+}
+
+
 NPM=$( npm | grep "command not found")
 echo "$NPM"
 if [[ -n $NPM ]]; then
@@ -13,10 +21,13 @@ if [[ -n $NEWMAN ]]; then
 fi
 
 path="$(pwd)/$(dirname  $0 )"
+clearDB
 
 collection=$(find "$path" -name "*environment.json")
 for file in $path/*collection.json
 do
   echo "Executing test $file"
   newman run "$file" -e "$collection" --bail
+  clearDB
 done
+
