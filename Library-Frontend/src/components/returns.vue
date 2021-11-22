74b46bc224aa7288d57ee131c24ac7ed2c6f05c0
Hana Gustyn <hanagustyn@gmail.com>
2021-11-22 15:33:17 -0500

    Initial frontend changes

---
+++ Library-Frontend/src/components/returns.vue
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+<template>
+    <div id="returns">
+        <md-table md-card>
+            <md-table-row>
+                <md-table-head>Items on Loan</md-table-head>
+                <md-table-head>Customer</md-table-head>
+                <md-table-head>Return Date</md-table-head>
+            </md-table-row>
+        </md-table>
+        <md-table md-card>
+            <md-table-row>
+                <md-table-head>Items on Hold</md-table-head>
+                <md-table-head>Customer</md-table-head>
+                <md-table-head>Last Pickup Day</md-table-head>
+            </md-table-row>
+        </md-table>
+    </div>
+</template>
+
+<style>
+</style>
