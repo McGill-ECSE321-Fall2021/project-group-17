5380f91440578c39d7cd1141bab61819f479d608
Jerry Xia <jerrylxia@gmail.com>
2021-11-22 17:04:47 -0500

    Skeleton from tutorial

--- Library-Frontend/src/router/index.js
+++ Library-Frontend/src/router/index.js
@@ -1,21 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import reservation from '@/components/reservation'
+import Vue from "vue";
+import Router from "vue-router";
+import Hello from "@/components/Hello";
+import reservation from "@/components/reservation";
+import shifts from "@/components/shifts";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
+      path: "/",
+      name: "Hello",
       component: Hello
     },
     {
-      path: '/app',
-      name: 'EventRegistration',
+      path: "/app",
+      name: "EventRegistration",
       component: reservation
+    },
+    {
+      path: "/shifts",
+      name: "EventRegistration",
+      component: shifts
     }
   ]
-})
+});
