01d3b98b959eca32143fb2b25bb01bb4645068c5
Jerry Xia <jerrylxia@gmail.com>
2021-11-23 13:26:47 -0500

    Add working get librarians functionality to backend

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/LibrarianService.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/LibrarianService.java
@@ -60,11 +60,9 @@ public class LibrarianService {
     }
     @Transactional
     public List<PersonRole> getAllLibrarians() {
-    	@SuppressWarnings("unchecked")
-		Iterator<PersonRole> iter = (Iterator<PersonRole>) librarianRepository.findAll();
         List<PersonRole> librarians = new ArrayList<PersonRole>();
-        while (iter.hasNext())
-        	librarians.add(iter.next());
+		Iterable<PersonRole> iter = librarianRepository.findAll();
+    	iter.forEach(librarians::add);
         return librarians;
     }
     @Transactional
