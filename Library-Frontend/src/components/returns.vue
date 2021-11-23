839d20baa94139cc17988a6f38242a5c43260eb2
Hana Gustyn <hanagustyn@gmail.com>
2021-11-22 19:44:53 -0500

    Added table to returns frontend

---
+++ Library-Frontend/src/components/returns.vue
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+import VueMaterial from 'vue-material'
+
+Vue.use(VueMaterial)
+
+<template>
+    <div id="returns">
+        <h3>Returns</h3>
+        <table class="center">
+            <tr>
+                <th>Items on Loan</th>
+                <th>Customer</th>
+                <th>Return Date</th>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<style>
+    h3 {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .center {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    th {
+        padding: 20px;
+    }
+</style>
--- Library-Frontend/src/router/index.js
+++ Library-Frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import reservation from '@/components/reservation'
+import returns from '@/components/returns'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/app',
       name: 'EventRegistration',
       component: reservation
+    },
+    {
+      path: '/returns',
+      name: 'Returns',
+      component: returns
     }
   ]
 })
