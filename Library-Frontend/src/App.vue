a778f241b35497c39d76fc3d814f76056b096001
Tomasz Mroz <tomasz.mroz@mail.mcgill.ca>
2021-11-29 14:23:55 -0500

    more active item stuff

--- Library-Frontend/src/App.vue
+++ Library-Frontend/src/App.vue
@@ -1,15 +1,54 @@
 <template>
   <div id="app">
-<!--    <div id="LMS">
-      <h1>Library Management System</h1>
-      <b-row>
-        <md-button to="/homepage" class="md-icon-button">
-          <md-icon>home</md-icon>
-        </md-button>
-      <md-button @click="logout()" class="create-button">Logout</md-button>
-      </b-row>
+    <div v-show="this.$cookie.get('customerId')">
+      <b-navbar toggleable="md" type="light" variant="light" class="list-unstyled">
+        <b-navbar variant="faded" type="light" left>
+          <b-navbar-brand >
+            <img src="https://i.ibb.co/L5shFK6/LMS-removebg-preview.png" alt="Logo" height="50" width="60">
+          </b-navbar-brand>
+        </b-navbar>
+        <b-nav-item @click="iteminstance()">
+          <a class="text-my-own-color ">
+            Find movies, books, and music
+          </a>
 
-    </div>-->
+        </b-nav-item>
+        <b-nav-item href="#/active" class="unstyled">
+          <a class="text-my-own-color ">
+            View Active Loans
+          </a>
+        </b-nav-item >
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-input-group prepend="What are you looking for today?">
+              <b-form-input size="md" class="mr-sm-2" ></b-form-input>
+            </b-input-group>
+          </b-nav-form>
+
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <img src="https://icons-for-free.com/iconfiles/png/512/person+user+icon-1320166085409390336.png" alt="Logo" height="30" width="30">
+            </template>
+            <b-dropdown-item @click="profile()">Profile</b-dropdown-item>
+            <b-dropdown-item @click="logout()">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-navbar>
+      <nav class="navbar fixed-bottom navbar-light" style="background-color: #76323F;">
+        <button class="left" @click="homepage()">
+          <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" alt="Logo" height="30" width="30">
+        </button>
+      </nav>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -21,6 +60,18 @@ export default {
     logout: function () {
       this.$cookie.delete('customerId');
       this.$router.push({name: 'login'});
+    },
+    profile(){
+        this.$router.push({name:'Profile'});
+      },
+    iteminstance(){
+        this.$router.push({name:'Item Instance'});
+      },
+    login(){
+        this.$router.push({name:'login'});
+      },
+    homepage(){
+        this.$router.push({name: 'HomePage'});
     }
   }
 }
@@ -35,13 +86,18 @@ export default {
 @import "~vue-material/dist/theme/engine.scss";
 
 @include md-register-theme("default", (
-  primary: #76323F, // The primary color of your application
-  accent: #C09F80 // The accent or secondary color
+primary: #76323F, // The primary color of your application
+accent: #C09F80 // The accent or secondary color
 ));
 
 @import "~vue-material/dist/theme/all";
 
 
+.text-my-own-color {
+  font-size:18px;
+  font-family:monospace;
+    color: black;
+}
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
--- Library-Frontend/src/components/active-items.vue
+++ Library-Frontend/src/components/active-items.vue
@@ -1,38 +1,34 @@
 import Vue from 'vue'
 <template>
-  <div id="app">
-    <div class="signup-page">
-      <div class="container">
-        <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
-          <div class="card signup">
-            <md-tabs class="md-primary" md-alignment="centered">
-              <md-tab id="tab-home" md-label="Active Reservations">
-                <md-table>
-                  <md-table v-model="searchedReservations" md-card md-fixed-header md-sort-order="asc">
-                    <md-table-toolbar>
-                      <md-field md-clearable class="md-toolbar-section-end">
-                        <md-input placeholder="Search by name..." v-model="search" @input="searchOnTableReservation" />
-                      </md-field>
-                    </md-table-toolbar>
+  <div id="wrapper" class="overlay">
+      <div id="table" class="layer2">
+          <md-tabs class="md-primary" md-alignment="centered">
+            <md-tab id="tab-home" md-label="Active Reservations">
+              <md-table>
+                <md-table v-model="searchedReservations" md-card md-fixed-header md-sort-order="asc">
+                  <md-table-toolbar>
+                    <md-field md-clearable class="md-toolbar-section-end">
+                      <md-input placeholder="Search by name..." v-model="search" @input="searchOnTableReservation" />
+                    </md-field>
+                  </md-table-toolbar>
 
-                    <md-table-empty-state
-                      md-label="No Reservations found"
-                      :md-description="`No reservations found for this search query. Try a different search term.`">
-                    </md-table-empty-state>
+                  <md-table-empty-state
+                    md-label="No Reservations found"
+                    :md-description="`No reservations found for this search query. Try a different search term.`">
+                  </md-table-empty-state>
 
-                    <md-table-row slot="md-table-row" slot-scope="{ item }" class="md-primary" md-selectable="single">
-                      <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
-                      <md-table-cell md-label="Date Reserved" md-sort-by="datePublished">{{ item.dateReserved }}</md-table-cell>
-                      <md-table-cell md-label="Pickup Date" md-sort-by="name">{{ item.pickupDay }}</md-table-cell>
-                      <md-table-cell md-label="Name of Item" md-sort-by="creator">{{ item.itemInstance.checkableItem.name }}</md-table-cell>
-                    </md-table-row>
-                  </md-table>
+                  <md-table-row slot="md-table-row" slot-scope="{ item }" class="md-primary" md-selectable="single">
+                    <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
+                    <md-table-cell md-label="Date Reserved" md-sort-by="datePublished">{{ item.dateReserved }}</md-table-cell>
+                    <md-table-cell md-label="Pickup Date" md-sort-by="name">{{ item.pickupDay }}</md-table-cell>
+                    <md-table-cell md-label="Name of Item" md-sort-by="creator">{{ item.itemInstance.checkableItem.name }}</md-table-cell>
+                  </md-table-row>
                 </md-table>
-                <md-button class="create-button" @click="updateReservation()">Update Reservation</md-button>
-                <md-button class="create-button" @click="deleteReservation">Delete Reservation</md-button>
-              </md-tab>
-              <md-tab id="tab-pages" md-label="Active Loans">
-                <md-table v-model="searchedLoans" md-card md-fixed-header md-sort-order="asc" @md-selected="onSelectReservation">
+              </md-table>
+            </md-tab>
+            <md-tab id="tab-pages" md-label="Active Loans">
+              <md-table>
+                <md-table v-model="searchedLoans" md-card md-fixed-header md-sort-order="asc">
                   <md-table-toolbar>
                     <md-field md-clearable class="md-toolbar-section-end">
                       <md-input placeholder="Search by name..." v-model="search" @input="searchOnTableLoan" />
@@ -51,21 +47,18 @@ import Vue from 'vue'
                     <md-table-cell md-label="Name of Item" md-sort-by="creator">{{ item.itemInstance.checkableItem.name }}</md-table-cell>
                   </md-table-row>
                 </md-table>
-
-              </md-tab>
-            </md-tabs>
-          </div>
-        </div>
+              </md-table>
+            </md-tab>
+          </md-tabs>
       </div>
-    </div>
   </div>
 </template>
 
 
 <script src="./active-items.js">
 </script>
+<style>
 
-<style scoped>
 p {
   line-height: 1rem;
 }
@@ -93,44 +86,26 @@ p {
   font-size: 0.8rem;
 
 }
-
-.signup-input {
-  margin-top: 15px;
-  margin-left: 10px;
-  height: 25px
+#reservation {
+  font-family: monospace;
+  font-size: 24px;
+  outline: dashed 1px black;
+  background-color: #D7CeC7;
+  /* Center child horizontally*/
+  justify-content: center;
+  flex-grow : 1;
+  display: grid
 }
-
-.search{
-  margin-right: 20px;
-  height: 25px;
-  margin-top: 20px;
-}
-.btn {
-  width: 100px;
-  height: 25px;
-  margin-bottom: 20px;
-  margin-top: 20px;
-  color: #D7CEC7;
-  background: #76323f;
-  text-align: center;
-  align-items: center;
-}
-
-.signup-page {
-  align-items: center;
+#wrapper {
+  background-color: #565656;
   display: flex;
-  height: 100vh;
-  background: url(https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80);
+  flex-flow: column;
+  height: 100%;
+  padding: 3% 7.5% 10%;
 }
-
-h1 {
-  margin-bottom: 20px;
-}
-
-
-.address-label {
-  margin-top: 30px;
-  margin-bottom: -15px;
+.layer2{
+  grid-column: 1;
+  grid-row: 2;
 }
 
 
