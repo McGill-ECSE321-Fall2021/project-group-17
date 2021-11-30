f9c924b53090c1ff35f5f9aba035ee9ce7a00124
Victoria Sanchez <victoria.sanchez@mail.mcgill.ca>
2021-11-30 12:20:00 -0500

    adding library card functionality

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/LibraryCardService.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/LibraryCardService.java
@@ -1,7 +1,10 @@
 package ca.mcgill.ecse321.library.service;
 
+import ca.mcgill.ecse321.library.dao.CustomerRepository;
 import ca.mcgill.ecse321.library.dao.LibraryCardRepository;
+import ca.mcgill.ecse321.library.model.Customer;
 import ca.mcgill.ecse321.library.model.LibraryCard;
+import ca.mcgill.ecse321.library.service.Exception.CustomerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +14,18 @@ public class LibraryCardService {
 
     @Autowired
     LibraryCardRepository libraryCardRepository;
-
+    @Autowired
+    CustomerRepository customerRepository;
     @Transactional
     public LibraryCard createLibraryCard(int id) {
+        Customer customer= (Customer) customerRepository.findPersonRoleById(id);
+        if(customer==null){
+            throw new CustomerException("customer does not exist");
+        }
         LibraryCard libraryCard = new LibraryCard();
-        libraryCard.setId(id);
+        libraryCard.setCustomer(customer);
+        customer.setLibraryCard(libraryCard);
+        customerRepository.save(customer);
         libraryCardRepository.save(libraryCard);
         return libraryCard;
     }
