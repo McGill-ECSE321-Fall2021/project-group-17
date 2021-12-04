48dc4bffe9fe8fbeff224da74145f2b9100cdbe2
Hana Gustyn <hanagustyn@gmail.com>
2021-12-04 11:07:56 -0500

    More changes to SignUpActivity

--- Android-Frontend/app/src/main/java/ca/mcgill/ecse321/library/SignUpActivity.java
+++ Android-Frontend/app/src/main/java/ca/mcgill/ecse321/library/SignUpActivity.java
@@ -17,6 +17,17 @@ import cz.msebera.android.httpclient.entity.mime.Header;
 
 public class SignUpActivity extends MainActivity {
     private String error = null;
+    private Integer customerId;
+    private Integer personId;
+    private Integer addressId;
+    private final String name = String.valueOf(findViewById(R.id.person_name));
+    private final String username = String.valueOf(findViewById(R.id.signup_username));
+    private final String password = String.valueOf(findViewById(R.id.signup_password));
+    private final String email = String.valueOf(findViewById(R.id.email_address));
+    private final String streetNumber = String.valueOf(findViewById(R.id.street_number));
+    private final String streetName = String.valueOf(findViewById(R.id.street_name));
+    private final String city = String.valueOf(findViewById(R.id.city));
+    private final String country = String.valueOf(findViewById(R.id.country));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +38,6 @@ public class SignUpActivity extends MainActivity {
     public void signUp(View v) throws JSONException {
         error = "";
 
-        final String name = String.valueOf(findViewById(R.id.person_name));
-        final String username = String.valueOf(findViewById(R.id.signup_username));
-        final String password = String.valueOf(findViewById(R.id.signup_password));
-        final String email = String.valueOf(findViewById(R.id.email_address));
-        final String streetNumber = String.valueOf(findViewById(R.id.street_number));
-        final String streetName = String.valueOf(findViewById(R.id.street_name));
-        final String city = String.valueOf(findViewById(R.id.city));
-        final String country = String.valueOf(findViewById(R.id.country));
-
         JSONObject jsonPerson = new JSONObject();
         jsonPerson.put("name", name);
         jsonPerson.put("personRole", "customer");
@@ -45,90 +47,27 @@ public class SignUpActivity extends MainActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
                     if (response.get("id").toString() != null) {
-                        int personId = Integer.parseInt(response.get("id").toString());
-
+                        personId = Integer.parseInt(response.get("id").toString());
 
                         HttpUtils.post("address/1/" + streetNumber + '/' + streetName + '/' + city + '/' + country, new RequestParams(), new JsonHttpResponseHandler() {
 
                             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                                 try {
-                                    int addressId = Integer.parseInt(response.get("id").toString());
+                                    addressId = Integer.parseInt(response.get("id").toString());
 
-                                    HttpUtils.post("customer/1/" + personId + "/0/" + addressId, new RequestParams(), new JsonHttpResponseHandler() {
+                                    createCustomer(v);
 
-                                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                                            try {
-                                                int customerId = Integer.parseInt(response.get("id").toString());
-
-                                                HttpUtils.post("librarycard/" + customerId, new RequestParams(), new JsonHttpResponseHandler() {
-
-                                                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                                                        try {
-
-                                                            RequestParams params = new RequestParams();
-                                                            params.add("personRoleId", String.valueOf(customerId));
-
-                                                            HttpUtils.post("onlineaccount/customer/" + username + '/' + password + '/' + email, params, new JsonHttpResponseHandler() {
-
-                                                                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                                                                    try {
-                                                                        HttpUtils.post("customer/" + customerId + '/' + username, new RequestParams(), new JsonHttpResponseHandler() {
-
-                                                                            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                                                                                try {
-
-
-                                                                                } catch (Exception e) {
-
-                                                                                }
-                                                                            }
-
-
-                                                                            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
-                                                                            }
-                                                                        });
-
-                                                                    } catch (Exception e) {
-
-                                                                    }
-                                                                }
-
-
-                                                                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
-                                                                }
-                                                            });
-
-                                                        } catch (Exception e) {
-
-                                                        }
-                                                    }
-
-
-                                                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
-                                                    }
-                                                });
-
-                                            } catch (Exception e) {
-
-                                            }
-                                        }
-
-
-                                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
-                                        }
-                                    });
                                 } catch (Exception e) {
 
                                 }
                             }
 
-
                             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
+                                try {
+                                    error = errorResponse.get("message").toString();
+                                } catch (JSONException e) {
+                                    error = e.getMessage();
+                                }
                             }
                         });
 
@@ -139,10 +78,105 @@ public class SignUpActivity extends MainActivity {
             }
 
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
+                try {
+                    error = errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error = e.getMessage();
+                }
             }
         });
 
+
+    }
+
+    private void createLibraryCard(View v) {
+        HttpUtils.post("librarycard/" + customerId, new RequestParams(), new JsonHttpResponseHandler() {
+
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                createOnlineAccount(v);
+            }
+
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error = errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error = e.getMessage();
+                }
+            }
+        });
+    }
+
+    private void createCustomer(View v) {
+        HttpUtils.post("customer/1/" + personId + "/0/" + addressId, new RequestParams(), new JsonHttpResponseHandler() {
+
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    customerId = Integer.parseInt(response.get("id").toString());
+
+                    createLibraryCard(v);
+
+                } catch (Exception e) {
+
+                }
+            }
+
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error = errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error = e.getMessage();
+                }
+            }
+        });
+    }
+
+    private void createOnlineAccount(View v) {
+        RequestParams params = new RequestParams();
+        params.add("personRoleId", String.valueOf(customerId));
+
+        HttpUtils.post("onlineaccount/customer/" + username + '/' + password + '/' + email, params, new JsonHttpResponseHandler() {
+
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    HttpUtils.post("customer/" + customerId + '/' + username, new RequestParams(), new JsonHttpResponseHandler() {
+
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            try {
+                                login(v);
+
+                            } catch (Exception e) {
+
+                            }
+                        }
+
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            try {
+                                error = errorResponse.get("message").toString();
+                            } catch (JSONException e) {
+                                error = e.getMessage();
+                            }
+                        }
+                    });
+
+                } catch (Exception e) {
+
+                }
+            }
+
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error = errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error = e.getMessage();
+                }
+            }
+        });
+    }
+
+    public void login(View v) {
         HttpUtils.post("login/" + username + '/' + password, new RequestParams(), new JsonHttpResponseHandler() {
 
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -150,7 +184,11 @@ public class SignUpActivity extends MainActivity {
             }
 
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-
+                try {
+                    error = errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error = e.getMessage();
+                }
             }
         });
     }
