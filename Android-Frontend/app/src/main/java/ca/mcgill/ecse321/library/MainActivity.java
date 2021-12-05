9ffb805cfcf3c0703b75a1bda3d4e4bba68a03cd
Victoria Sanchez <victoria.sanchez@mail.mcgill.ca>
2021-12-04 22:47:44 -0500

    fixes

--- Android-Frontend/app/src/main/java/ca/mcgill/ecse321/library/MainActivity.java
+++ Android-Frontend/app/src/main/java/ca/mcgill/ecse321/library/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_homepage);
         /*
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
